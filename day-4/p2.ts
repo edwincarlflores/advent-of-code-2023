b2da54c86732cebd8a50fa916f69187792726673
Edwin Carl Flores <edwincarlflores@gmail.com>
2023-12-16 07:01:00 +1300

    Complete day 4 part 2

--- day-4/p2.ts
+++ day-4/p2.ts
@@ -1,34 +1,10 @@
 import { getLines } from "../utils";
 
-const lines = await getLines("./inputs/test.txt");
-// const lines = await getLines("./inputs/input.txt");
+// const lines = await getLines("./inputs/test.txt");
+const lines = await getLines("./inputs/input.txt");
 
-const allCards: number[] = [];
-const initialCards: number[] = [];
-const cardsWonLookup = new Map<number, number[]>();
-
-function getCardsWon(cardNumber: number, matchLength: number) {
-  const cardsWon: number[] = [];
-  for (let idx = 1; idx <= matchLength; idx++) {
-    cardsWon.push(cardNumber + idx);
-  }
-  return cardsWon;
-}
-
-function addAllCardsWon(cardNumber: number) {
-  const cardsWon = cardsWonLookup.get(cardNumber);
-  if (!cardsWon || cardsWon.length === 0) {
-    return;
-  }
-
-  for (const cardWon of cardsWon) {
-    allCards.push(cardWon);
-    addAllCardsWon(cardWon);
-  }
-}
-
-for (const line of lines) {
-  const cardNumber = parseInt(line.split(":")[0].trim().split(" ")[1]);
+function getCardsWon(cardNumber: number) {
+  const line = lines[cardNumber - 1];
   const numbers = line.split(":")[1].trim().split(" | ");
   const winningNumbers = numbers[0]
     .split(" ")
@@ -42,21 +18,37 @@ for (const line of lines) {
     ownNumbers.includes(winningNumber),
   );
 
-  initialCards.push(cardNumber);
-
-  cardsWonLookup.set(
-    cardNumber,
-    getCardsWon(cardNumber, ownWinningNumbers.length),
-  );
+  const cardsWon: number[] = [];
+  for (let idx = 1; idx <= ownWinningNumbers.length; idx++) {
+    cardsWon.push(cardNumber + idx);
+  }
+  return cardsWon;
 }
 
-console.log(cardsWonLookup);
-// console.log(initialCards);
+const toProcess = new Array(lines.length).fill(0).map((_, i) => i + 1);
+const lookup: Record<number, number[]> = {};
+const frequencies: Record<number, number> = {};
 
-for (const card of initialCards) {
-  allCards.push(card);
-  addAllCardsWon(card);
+while (toProcess.length) {
+  const cardNum = toProcess.pop();
+
+  if (!cardNum) {
+    break;
+  }
+
+  frequencies[cardNum] = frequencies[cardNum] ? frequencies[cardNum] + 1 : 1;
+  const points = lookup[cardNum] ? lookup[cardNum] : getCardsWon(cardNum);
+
+  if (!lookup[cardNum]) {
+    lookup[cardNum] = points;
+  }
+
+  toProcess.push(...points);
 }
 
-console.log(allCards);
-console.log(allCards.length);
+const totalCardsCount = Object.entries(frequencies).reduce(
+  (acc, [, count]) => acc + count,
+  0,
+);
+
+console.log(totalCardsCount);
