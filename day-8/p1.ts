7c453a5d760ef8ddf44091549fc74f68742b2a78
Edwin Carl Flores <edwincarlflores@gmail.com>
2023-12-26 13:19:29 +1300

    complete day 7 part 1

---
+++ day-8/p1.ts
@@ -0,0 +1,37 @@
+import { getLines } from "../utils";
+
+// const lines = await getLines("./inputs/test.txt");
+// const lines = await getLines("./inputs/test2.txt");
+const lines = await getLines("./inputs/input.txt");
+
+const map: Record<string, [string, string]> = {};
+const instructions = lines.shift();
+if (!instructions) {
+  throw new Error("Missing instructions");
+}
+
+for (const line of lines) {
+  if (line === "") {
+    continue;
+  }
+
+  const currentNode = line.split(" = ")[0].trim();
+  const neighbors = line.split(" = ")[1].trim().slice(1, -1).split(", ") as [
+    string,
+    string,
+  ];
+
+  map[currentNode] = neighbors;
+}
+
+let steps = 0;
+let current = "AAA";
+const target = "ZZZ";
+
+while (current !== target) {
+  const idx = instructions[steps % instructions.length] === "L" ? 0 : 1;
+  current = map[current][idx];
+  steps++;
+}
+
+console.log(steps);
